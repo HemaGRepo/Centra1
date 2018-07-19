e7caeaa96ec600c8f9ee923970c015651c644676
Hema G <hema.g@capgemini.com>
2018-07-19 16:17:27 +0530

    V3.0

--- JulyDemo/src/PMSMain.java
+++ JulyDemo/src/PMSMain.java
@@ -4,6 +4,7 @@ public class PMSMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Project Management System");
 		System.out.println("Copyright Capgemini");
+		System.out.println("Thank U");
 	}
 
 }
