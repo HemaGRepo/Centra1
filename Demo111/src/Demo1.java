1dc31781e35872a60ed142d4b02fa1518ab02fbf
Hema G <hema.g@capgemini.com>
2018-07-19 16:28:16 +0530

    V1.0

---
+++ Demo111/src/Demo1.java
@@ -0,0 +1,11 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Project Management System");
+		System.out.println("Copyright Capgemini");
+		System.out.println("Thank U");
+
+	}
+
+}
